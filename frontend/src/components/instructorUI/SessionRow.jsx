e724b134fb5caeac0dd8a1c50f4666fe5e0d5869
dragomirNonov <90495668+dragomirNonov@users.noreply.github.com>
2024-11-18 17:42:29 -0600

    changed the instructor view of StudentHours to match the manager view

---
+++ frontend/src/components/instructorUI/SessionRow.jsx
@@ -0,0 +1,25 @@
+const SessionRow = ({ session }) => {
+  return (
+    <tr className="border-b border-gray-200">
+      <td className="py-1 md:px-2">
+        <input
+          type="date"
+          id="date"
+          name="date"
+          value={session.date}
+          className="p-1 max-w-[85%]"
+          disabled
+        />
+      </td>
+      <td className="py-1 md:px-2">
+        <td className="py-1 md:px-2">{session.clockedIn}</td>
+      </td>
+      <td className="py-1 md:px-2">
+        <td className="py-1 md:px-2">{session.clockedOut}</td>
+      </td>
+      <td className="py-1 md:px-2">{session.duration} hr(s)</td>
+    </tr>
+  );
+};
+
+export default SessionRow;
